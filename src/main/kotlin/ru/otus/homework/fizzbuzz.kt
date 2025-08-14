f5c0f1a714369a97d28f4b021e60a787038b07f9
Dmitriy Bulygin <demetrius.storm@gmail.com>
2025-08-14 15:39:48 +0300

    added solution to the problem fizzbuzz

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,27 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    // создаём массив Integers, размером n
+    var i = 0
+    val arrOfIntegers = Array(n) { i++ }
+
+    // создаём массив Strings, размером n
+    val arrOfStrings = Array(n) {""} //emptyArray<String>()   //
+
+    // итерация по массиву arrOfIntegers, наполняем массив согласно заданию
+    for ((i, v) in arrOfIntegers.withIndex()) {
+        val value: String = if (i == 0 || (i % 3 == 0 && i % 5 == 0)) {
+            "FizzBuzz"
+        } else if (i % 3 == 0) {
+            "Fizz"
+        } else if (i % 5 == 0) {
+            "Buzz"
+        } else {
+            v.toString()
+        }
+
+        arrOfStrings[i] = value
+    }
+
+    return arrOfStrings
 }
